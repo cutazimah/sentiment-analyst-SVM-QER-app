4dd4d14e3db3e72bbc0211fb19481120e4345621
cutazimah <120536148+cutazimah@users.noreply.github.com>
2025-07-01 20:02:39 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from PIL import Image
 import base64
-import os
 
 
 def app():
@@ -9,32 +8,54 @@ def app():
     col1, col2 = st.columns(2)
 
     with col1:
+        # st.subheader("Tentang Aplikasi ")
         st.subheader("Deskripsi aplikasi")
         st.markdown(
             """
-            <style>
-                .justified-text {
-                    text-align: justify;
-                }
-            </style>
-            <div class="justified-text">
-            Aplikasi Web Sentimen Analisis ini diperuntukkan dalam melihat sentimen kepuasan pelanggan 
-            dalam menggunakan aplikasi jasa. Pembuatan modelnya menggunakan data training yang diambil 
-            dari twitter pada salah satu aplikasi ojek online, @GrabID. Algoritma yang digunakan 
-            dalam sentimen analisis ini ialah Support Vector Machine (SVM) dan seleksi fitur melalui penekanan
-            pembobotan fitur menggunakan Query Expansion Rangking (QER). 
-            </div>
-            """,
+                <style>
+                    .justified-text {
+                        text-align: justify;
+                    }
+                </style>
+                <div class="justified-text">
+                Aplikasi Web Sentimen Analisis ini diperuntukkan dalam melihat sentimen kepuasan pelanggan 
+                dalam menggunakan aplikasi jasa. Algoritma yang digunakan dalam sentimen analisis ini ialah Support Vector Machine (SVM) dan seleksi fitur menggunakan Query Expansion Rangking (QER).
+                Sistem ini akan mengklasifikasikan sebuah teks maupun dokumen kepada 3 kelas yang tersedia yaitu, negatif, netral dan positif. 
+                
+                Ada dua fitur yang ditawarkan, yaitu :
+                1. Klasifikasi dokumen = Menampilkan  sebuah dashboard informasi dokumen dan hasil klasifikasi 
+                2. Klasifikasi teks = Menampilkan hasil klasifikasi teks dan data bersih
+
+                </div>
+                """,
             unsafe_allow_html=True)
 
+        with st.expander("**PENJELASAN PENGGUNAAN APLIKASI**"):
+            st.markdown(
+                """
+                <style>
+                    .justified-text {
+                        text-align: justify;
+                    }
+                </style>
+                <div class="justified-text"> 
+
+                🎯 Cara penggunaan fitur **klasifikasi dokumen** adalah dengan memasukkan dokumen file (CSV/Excel), lalu selanjutnya 
+                menunggu file di proses selama 1 - 3 menit, sesuai dengan *ukuran file*. Selanjutnya akan tampil :red-background[dashboard] 
+                mulai dari rentang waktu, distribusi kelas sentimen dan tabel  hasil klasifikasi.
+
+                📌 Cara penggunaan fitur **klasifikasi teks** adalah dengan memasukkan inputan cuitan X tentang kepuasan layanan bagian jasa,
+                maka sistem akan memproses teks. Hasil dari proses ini adalah :red-background[data bersih] dan :red-background[label sentimen]
+                </div>
+                """,
+                unsafe_allow_html=True)
+
     with col2:
-        # Gunakan path relatif, agar bisa dibaca di Streamlit Cloud
-        image_path_2 = os.path.join(os.path.dirname(__file__), "emotional.png")
 
-        with open(image_path_2, "rb") as img_file:
-           img_base64 = base64.b64encode(img_file.read()).decode()
+        with open("emotional.png", "rb") as img_file:
+            img_base64 = base64.b64encode(img_file.read()).decode()
 
-        st.markdown(
+            st.markdown(
                 f"""
         <style>
         .center-image {{
